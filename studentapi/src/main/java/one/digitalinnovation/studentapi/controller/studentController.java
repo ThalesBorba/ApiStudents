e840d1692585c1fcc30b0a2da37c8a5a8fe09056
ThalesBorba <thales_borba@hotmail.com>
2021-10-31 21:39:17 -0300

    update

--- studentapi/src/main/java/one/digitalinnovation/studentapi/controller/studentController.java
+++ studentapi/src/main/java/one/digitalinnovation/studentapi/controller/studentController.java
@@ -1,5 +1,6 @@
 package one.digitalinnovation.studentapi.controller;
 
+import one.digitalinnovation.studentapi.entities.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
